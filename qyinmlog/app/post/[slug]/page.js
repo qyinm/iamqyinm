8dcaed6240fb37dc39a4eb02a51099ed39f9040c
seungwoo <80995345+iamseungwoo@users.noreply.github.com>
2023-11-29 10:45:55 +0000

    feat: post slug page

---
+++ qyinmlog/app/post/[slug]/page.js
@@ -0,0 +1,33 @@
+import { compileMDX } from 'next-mdx-remote/rsc';
+import { notFound } from 'next/navigation';
+import Giscus from '@/app/components/global/Giscus';
+import PostTitle from '@/app/components/post/PostTitle';
+import { getAllPosts } from '@/app/api/post';
+
+const Page = async ({ params }) => {
+  const slug = `/post/${[decodeURI(params.slug)].join('/')}`;
+  const post = getAllPosts().find(post => post.slug === slug);
+
+  console.log();
+  if (!post) {
+    return notFound();
+  }
+
+  const { content, frontmatter } = await compileMDX({
+    source: post.content,
+    options: {
+      parseFrontmatter: true,
+    },
+  });
+  return (
+    <>
+      <PostTitle post={post} />
+      <article className="prose dark:prose-dark max-w-none my-12 prose-neutral font-spoqa dark:prose-dark">
+        {content}
+      </article>
+      <Giscus />
+    </>
+  );
+};
+
+export default Page;
