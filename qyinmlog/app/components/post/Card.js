7157cd39f5442c9b591b3acda7c639dbcbd8a099
seungwoo <80995345+iamseungwoo@users.noreply.github.com>
2023-11-30 10:40:40 +0000

    feat: post card add thumbnail, reactive size

--- qyinmlog/app/components/post/Card.js
+++ qyinmlog/app/components/post/Card.js
@@ -1,30 +1,37 @@
 import Link from 'next/link';
 import PostInfo from './PostInfo';
 import Tag from './Tag';
-import Image from 'next/image';
+// import Image from 'next/image';
+// import TagBox from './tag/TagBox';
 
 const Card = ({ post }) => {
   const postLink = post.slug.replace('/posts/', `/article/`);
   console.log(post);
   return (
-    <div className="text-ye group w-full p-4 py-4 hover:drop-shadow-base">
-      <div className="flex justify-between">
-        <div className="w-full flex-col justify-around">
+    <div className="text-ye group w-full mt-3 p-4 py-4 hover:drop-shadow-base">
+      <div className="sm:flex justify-between bg-secondary rounded-lg">
+        <div className="sm:mr-4 sm:w-3/5 w-full">
+          <img
+            src={post.thumbnail}
+            className="sm:w-[32vw] sm:h-[24vh] sm:rounded-l-lg sm:rounded-r-none w-full rounded-t-lg"
+          />
+        </div>
+        <div className="mt-3 ml-3 w-full flex flex-col justify-around justify-items-center content-center">
+          <p></p>
           <Link className="hover:drop-shadow-base" href={postLink}>
             <p className="text-xl font-bold">{post.title}</p>
             <p className="text-tertiary mt-1"></p>
           </Link>
-          <div className="mt-2 inline-flex w-full items-start gap-2 text-sm">
+          <div className="my-2 sm:inline-flex w-full items-start gap-2 text-sm">
             <div className="flex flex-wrap items-center gap-2">
               {post.tags.map(tag => (
                 <Tag name={tag} />
               ))}
             </div>
-            <PostInfo date={post.date} readingTime={post.readingMinutes} />
+            <div className="mt-2">
+              <PostInfo date={post.date} readingTime={post.readingMinutes} />
+            </div>
           </div>
-        </div>
-        <div className="ml-4 w-3/5">
-          <img src={'/image' + post.thumbnail} className="w-[32vw] h-[24vh]" />
         </div>
       </div>
     </div>
--- qyinmlog/app/components/post/Tag.js
+++ qyinmlog/app/components/post/Tag.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Tag = ({ name }) => {
   return (
     <Link href={`/tags/${name}`}>
-      <div className="rounded-lg px-2 py-0.5 transition-colors bg-secondary hover:text-primary hover:bg-tertiary text-secondary font-normal">
+      <div className="rounded-lg px-2 py-0.5 transition-colors bg-stone-300 hover:text-primary hover:bg-tertiary text-secondary font-normal">
         {name}
       </div>
     </Link>
