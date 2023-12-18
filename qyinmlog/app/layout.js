e2a4e3ec51e5bafd00bbacc8a2b8973e1094e234
seungwoo <80995345+iamseungwoo@users.noreply.github.com>
2023-12-18 13:42:39 +0000

    docs: add adsense

--- qyinmlog/app/layout.js
+++ qyinmlog/app/layout.js
@@ -25,9 +25,24 @@ export default function RootLayout({ children }) {
           content="uN6r8wtwmM5jus_sDEYdaP06xZ0jF7NGQ4npFnd2Xnk"
         />
         {/* new domain qyinm.com */}
-        <meta name="google-site-verification" content="8QhudNtMbibg206kBXKG8AzhKlZUnxnBFKT-33cD6nw" />
+        <meta
+          name="google-site-verification"
+          content="8QhudNtMbibg206kBXKG8AzhKlZUnxnBFKT-33cD6nw"
+        />
         {/* naver search console */}
-        <meta name="naver-site-verification" content="a138ad1aed864a9ed637a3d4577e923463d55d7c" />
+        <meta
+          name="naver-site-verification"
+          content="a138ad1aed864a9ed637a3d4577e923463d55d7c"
+        />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2698685047331512"
+          crossorigin="anonymous"
+        ></script>
+        <title>qyinm log</title>
+        <meta name="description" content="qyinm log" />
+        <meta name="author" content="qyinm" />
+        <meta name="keyword" content="backend, java, jpa, spring" />
       </head>
       <body className={'mx-auto max-w-3xl px-6 lg:max-w-4xl lg:px-8'}>
         <Header />
